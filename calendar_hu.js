dbb12881d7035e926caf2151c7dcc3fb194115f3
Miklos Matyas <matyas.miklos@terrabox.hu>
2025-04-15 08:18:45 +0000

    fields

--- calendar_hu.js
+++ calendar_hu.js
@@ -1,4 +1,13 @@
 
-console.log("magyar naptár");
+// GHL naptár magyarítás
 
 document.querySelector('.widgets-step-1-container h4').textContent = "Válassz időpontot";
+document.querySelector('.btn .selected-slot > div').textContent = "Kiválaszt";
+
+document.querySelector('h4.text-info').textContent = "Kérlek add meg az adataidat";
+document.querySelector('#form-first_name label').textContent = "Keresztnév";
+document.querySelector('#form-last_name label').textContent = "Vezetéknév";
+document.querySelector('#form-phone label').textContent = "Telefonszám";
+document.querySelector('.calendar_notes label').textContent = "További információk";
+document.querySelector('.calendar_notes textarea').placeholder = "Megjegyzés a kezeléssel kapcsolatban";
+
