447d0b11f6e76a5ab33e811712015662fa458da2
Miklos Matyas <matyas.miklos@terrabox.hu>
2025-04-16 11:47:25 +0200

    Select button

--- calendar_hu.js
+++ calendar_hu.js
@@ -2,7 +2,17 @@
 // GHL naptár magyarítás
 
 document.querySelector('.widgets-step-1-container h4').textContent = "Válassz időpontot";
-document.querySelector('.btn .selected-slot > div').textContent = "Kiválaszt";
+
+// Összes .widgets-time-slot elem kiválasztása
+document.querySelectorAll('.widgets-time-slot').forEach(slot => {
+  slot.addEventListener('click', () => {
+    // Minden .btn .selected-slot > div elem szövegének átírása
+    document.querySelectorAll('.btn .selected-slot > div').forEach(elem => {
+      elem.textContent = "Kiválaszt";
+    });
+  });
+});
+
 
 document.querySelector('h4.text-info').textContent = "Kérlek add meg az adataidat";
 document.querySelector('#form-first_name label').textContent = "Keresztnév";
@@ -10,4 +20,5 @@ document.querySelector('#form-last_name label').textContent = "Vezetéknév";
 document.querySelector('#form-phone label').textContent = "Telefonszám";
 document.querySelector('.calendar_notes label').textContent = "További információk";
 document.querySelector('.calendar_notes textarea').placeholder = "Megjegyzés a kezeléssel kapcsolatban";
+
 
